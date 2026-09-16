5755e7bc92115ded80953ba7e893bafd34467d82
ZionnShowers <zionn.d.showers@gmail.com>
2026-09-16 10:04:38 -0700

    Peer Review

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ Challenge #2 - Guess It
 I used a switch statement to select the difficulty. I then used do while loops and while loops for validation. 
 If statements to make comparisons of numerical values. 
 I used TryParse for validation, ensuring the user input a number
-Peer Reviewer:
-Review: 
+Peer Reviewer: Zionn Showers
+Review: Code seems to run fine. However, it feels like the coding and drawio image is way more complicated than it could've been,
+creating entirely new variables instead reusing other variables. It also doesn't seem count how many tries it took to find the
+correct number.
 */
 
 Console.Clear();
